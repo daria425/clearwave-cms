23bbf057e9caefc12caaa1a0bc6a0cd4218b81a7
daria425 <darianaumova5@gmail.com>
2023-10-13 03:28:20 +0300

    Add index page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Index from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
