1d7a9318cd2b5e8005368bfafb073811fad77f68
daria425 <darianaumova5@gmail.com>
2023-10-22 22:08:23 +0300

    Add logout

--- src/pages/PageComponents/Header.jsx
+++ src/pages/PageComponents/Header.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
 export default function Header() {
+  const nav = useNavigate();
+  async function handleLogout() {
+    await fetch("http://localhost:3000/api/logout");
+    nav("/");
+  }
   return (
     <header>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </header>
   );
 }
