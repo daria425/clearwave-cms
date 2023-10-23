eb0be67d5695914ce2385b99a17c2d2d01ba47b0
daria425 <darianaumova5@gmail.com>
2023-10-23 03:24:30 +0300

    Add redirect on login component if user is logged in

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,6 +1,15 @@
 import LoginForm from "./components/LoginForm";
-
+import { useContext, useEffect } from "react";
+import { appContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 export default function Index() {
+  const nav = useNavigate();
+  const { userLogin } = useContext(appContext);
+  useEffect(() => {
+    if (userLogin) {
+      nav("/dashboard");
+    }
+  }, [userLogin]);
   return (
     <>
       <h1>hi</h1>
