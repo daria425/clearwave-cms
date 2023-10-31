01b236386a65f792aba392d668594b7f2a41b460
daria425 <darianaumova5@gmail.com>
2023-11-01 01:56:31 +0200

    Style text editor

--- src/pages/EditPost/components/PostDetails.jsx
+++ src/pages/EditPost/components/PostDetails.jsx
@@ -1,28 +1,28 @@
 import PostImages from "./PostImages";
 export default function PostDetails({ selectedPost }) {
   return (
-    <section>
+    <section className="post-details">
       <PostImages selectedPost={selectedPost} />
-      <h1>{selectedPost.title}</h1>
-      <h3>Subheadings:</h3>
+      <h1 className="post-details-title">{selectedPost.title}</h1>
+      <h3 className="post-details-label">Subheadings:</h3>
       {selectedPost.content.subheadings.length > 0 &&
       selectedPost.content.subheadings[0] !== ""
         ? selectedPost.content.subheadings.map((subheading, index) => (
             <p key={index}>{subheading}</p>
           ))
         : "No subheadings"}
-      <h3>Snippets:</h3>
+      <h3 className="post-details-label"> Snippets:</h3>
       {selectedPost.content.snippets.length > 0 &&
       selectedPost.content.snippets[0] !== ""
         ? selectedPost.content.snippets.map((snippet, index) => (
             <p key={index}>{snippet}</p>
           ))
         : "No snippets"}
-      <h3>Main Text:</h3>
+      <h3 className="post-details-label">Main Text:</h3>
       <p>{selectedPost.content.main_text}</p>
-      <h3>Category:</h3>
+      <h3 className="post-details-label">Category:</h3>
       <p>{selectedPost.category.name}</p>
-      <h3>Tags:</h3>
+      <h3 className="post-details-label">Tags:</h3>
       <ul>
         {selectedPost.tags.map((tag, index) => (
           <li key={index}>{tag}</li>
--- src/pages/EditPost/components/TextEditor.jsx
+++ src/pages/EditPost/components/TextEditor.jsx
@@ -4,11 +4,12 @@ export default function TextEditor({
   handleCloseTextEditor,
 }) {
   return (
-    <div className="edit-form-texteditor">
-      <label htmlFor="main_text">
-        Body text:
+    <div className="texteditor">
+      <h2 className="texteditor-title">{selectedPost.title}</h2>
+      <label htmlFor="main_text" className="texteditor-label-main">
+        Edit post content:
         <textarea
-          className="edit-form-textarea"
+          className="texteditor-textarea"
           name="content.main_text"
           onChange={(e) => {
             handleNestedTextChange(e);
