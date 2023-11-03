b6a3632a1998c436b2bd694ec92c7c4020bbe460
daria425 <darianaumova5@gmail.com>
2023-11-03 17:16:22 +0200

    Change summary view to use context

--- src/pages/EditPost/components/SummaryView.jsx
+++ src/pages/EditPost/components/SummaryView.jsx
@@ -1,76 +1,24 @@
 import EditForm from "./EditPostForm";
 import PostDetails from "./PostDetails";
 import Modal from "./Modal";
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { contentContext } from "../../../App";
 import { useState } from "react";
 export default function SummaryView() {
-  const [selectedPost, setSelectedPost] = useState(false);
-  const [responseLoading, setResponseLoading] = useState(false);
-  const { id } = useParams();
-  console.log(id);
-  const { blogPosts, postsLoading } = useContext(contentContext);
+  const {
+    selectedPost,
+    responseLoading,
+    handleArrayChange,
+    handleChange,
+    handleCheckbox,
+    handleDataUpdate,
+    handleNestedTextChange,
+    handleResponse,
+    handleSelectChange,
+    handleNestedArrayChange,
+  } = useOutletContext();
 
-  useEffect(() => {
-    if (!postsLoading) {
-      const post = blogPosts.find((post) => post._id === id);
-      setSelectedPost(post); // If post is not found, set an empty object
-    }
-  }, [blogPosts, id, postsLoading]);
-  function handleChange(e) {
-    setSelectedPost({ ...selectedPost, [e.target.name]: e.target.value });
-  }
-  function handleNestedArrayChange(e) {
-    const [firstProp, nestedProp] = e.target.name.split(".");
-    setSelectedPost({
-      ...selectedPost,
-      [firstProp]: {
-        ...selectedPost[firstProp],
-        [nestedProp]: [e.target.value],
-      },
-    });
-  }
-
-  function handleResponse() {
-    setResponseLoading(!responseLoading);
-  }
-  function handleNestedTextChange(e) {
-    const [firstProp, nestedProp] = e.target.name.split(".");
-    setSelectedPost({
-      ...selectedPost,
-      [firstProp]: {
-        ...selectedPost[firstProp],
-        [nestedProp]: e.target.value,
-      },
-    });
-  }
-  function handleSelectChange(arr, e) {
-    const categoryObj = arr.find((category) => category._id === e.target.value);
-    console.log(categoryObj);
-    setSelectedPost({
-      ...selectedPost,
-      category: categoryObj,
-    });
-  }
-
-  function handleCheckbox(e) {
-    setSelectedPost({
-      ...selectedPost,
-      is_published: e.target.checked ? true : false,
-    });
-  }
-  function handleArrayChange(e) {
-    //happens separate from form submission
-    const newTags = selectedPost.tags.map((tag) =>
-      tag === e.target.name ? e.target.value : tag
-    );
-    setSelectedPost({ ...selectedPost, tags: newTags });
-  }
-
-  function handleDataUpdate(newPost) {
-    setSelectedPost(newPost);
-  }
   if (!selectedPost) {
     return <p>Loading...</p>;
   }
