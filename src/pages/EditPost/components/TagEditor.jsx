79b4ec4b632d8ff74a533a284d9034832d6e1667
daria425 <darianaumova5@gmail.com>
2023-11-04 18:04:37 +0200

    Complete functionality of adding and removing tags

--- src/pages/EditPost/components/TagEditor.jsx
+++ src/pages/EditPost/components/TagEditor.jsx
@@ -61,6 +61,7 @@ export default function TagEditor({
         onClick={(e) => {
           e.preventDefault();
           handleTagUpdate(newTag, tagsToDelete);
+          handleTagEditor(e);
         }}
       >
         SAVE
